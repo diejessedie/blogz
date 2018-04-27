4d3cc6aaa6c427585e3bb9d69a43921f5ace98ce
Jess <jjohnson4927@gmail.com>
2018-04-27 00:08:57 -0500

    The whole shabang

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from wtforms import (Form, PasswordField, TextField, TextAreaField, validators, 
-StringField, SubmitField, BooleanField)
-from wtforms_alchemy import Email
-
-
+from wtforms import (Form, PasswordField, validators, StringField)
+from hashutils import check_pw_hash, make_pw_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:launchcode@localhost:8889/Blogz'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:launchcode@localhost:8889/Blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = "JCJhmvHSY9uURT2v"
 db = SQLAlchemy(app)
+
 
 class Blog(db.Model):
 
@@ -25,85 +24,122 @@ class Blog(db.Model):
         self.body = body
         self.owner = owner
 
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, password):
+    def __init__(self, username, email, password):
         self.email = email
         self.password = password
+        self.username = username
+
 
 class LoginForm(Form):
-    email = StringField('Email Address', [
-        validators.Email(message='Invalid Email address'),
-        validators.DataRequired(),
-        validators.Length(
-            min=6, max=35,
-            message='Must be between %(min)d and %(max)d characters.')
-    ])
+    email = StringField(
+        'Email Address', [
+            validators.Email(message='Invalid Email address'),
+            validators.DataRequired(),
+            validators.Length(
+                min=6,
+                max=35,
+                message='Must be between %(min)d and %(max)d characters.')
+        ])
     password = PasswordField('Password', [validators.DataRequired()])
 
+
 class RegistrationForm(Form):
-    
-    email = StringField('Email Address', [
-        validators.Email(message='Invalid Email address'),
+
+    username = StringField('Username', [
         validators.DataRequired(),
         validators.Length(
-            min=6, max=35,
+            min=6,
+            max=35,
             message='Must be between %(min)d and %(max)d characters.')
     ])
+    email = StringField(
+        'Email Address', [
+            validators.Email(message='Invalid Email address'),
+            validators.DataRequired(),
+            validators.Length(
+                min=6,
+                max=35,
+                message='Must be between %(min)d and %(max)d characters.')
+        ])
     password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match'),
-        validators.Length(min=8, max=35, 
-        message='Must be between %(min)d and %(max)d characters.')
+        validators.Length(
+            min=8,
+            max=35,
+            message='Must be between %(min)d and %(max)d characters.')
     ])
     confirm = PasswordField('Confirm Password')
 
+
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register']
+    allowed_routes = ['login', 'register', 'blogs']
     #check for email in session dictionary, before responding to requests
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
 
-@app.route("/register", methods=['GET','POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-    
-        email = str(form.email.data)
-        password = str(form.password.data)
-        
-        new_user = User(email, password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect('./blog')
-    return render_template('register.html', form=form, title="Register", endpoint='/register')
+    if request.method == 'POST':
 
-@app.route("/login", methods=['GET','POST'])
+        existing_user = User.query.filter_by(username=form.username.data).first()
+        existing_email = User.query.filter_by(email=form.email.data).first()
+
+        if existing_user or existing_email:
+            flash('User already exists!', 'error')
+
+        elif form.validate() and not existing_user and not existing_email:
+
+            username = str(form.username.data)
+            email = str(form.email.data)
+            password = str(form.password.data)
+            password = make_pw_hash(password)
+
+            new_user = User(username, email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            flash('Registered and logged in', "success")
+            return redirect('./blog')
+    return render_template(
+        'register.html', form=form, title="Register", endpoint='/register')
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == 'POST':
         email = str(form.email.data)
         password = str(form.password.data)
         user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
+        if user and check_pw_hash(password, user.password):
             session['email'] = email
             flash('Logged in', "success")
             return redirect('/')
         else:
             flash('Password incorrect, or user does not exist', 'error')
-    
-    return render_template('login.html', form=form, title="Log In", endpoint='/login')
+
+    return render_template(
+        'login.html', form=form, title="Log In", endpoint='/login')
+
 
 @app.route('/logout')
 def logout():
     del session['email']
-    return redirect('/')
+    return redirect('/login')
+
 
 @app.route("/newblog", methods=['POST', 'GET'])
 def newblog():
@@ -112,20 +148,26 @@ def newblog():
     if request.method == 'POST':
         blog_name = request.form['blog-name']
         blog_body = request.form['blog-body']
-        
+
         if blog_name == '' or blog_body == '':
             flash('Blog name and blog body required!', 'error')
-            return render_template('new_blog.html',title="Post a New Blog!", 
-            name=blog_name, body=blog_body)
-        
+            return render_template(
+                'new_blog.html',
+                title="Post a New Blog!",
+                name=blog_name,
+                body=blog_body)
+
         else:
             new_blog = Blog(blog_name, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()
-            
-            return render_template('single_post.html', blog=new_blog)
+            post_url = './post?id={0}'.format(new_blog.id)
 
-    return render_template('new_blog.html',title="Post a New Blog!", name=' ', body=' ')
+            return redirect(post_url)
+
+    return render_template(
+        'new_blog.html', title="Post a New Blog!", name=' ', body=' ')
+
 
 @app.route("/blog", methods=['GET'])
 @app.route('/', methods=['GET'])
@@ -134,8 +176,12 @@ def blogs():
     blogs = Blog.query.all()
     blogs = list(reversed(blogs))
 
-    return render_template('all_blogs.html',title="Blogs!", 
-        blogs=blogs, endpoint='/blog')
+    return render_template(
+        'all_blogs.html',
+        title="Blogs!",
+        blogs=blogs,
+        endpoint='/blog')
+
 
 @app.route("/myblog", methods=['GET'])
 def myblogs():
@@ -144,8 +190,32 @@ def myblogs():
     blogs = Blog.query.filter_by(owner=owner).all()
     blogs = list(reversed(blogs))
 
-    return render_template('all_blogs.html',title="My Blog!", 
-        blogs=blogs, endpoint='/myblog')
+    return render_template(
+        'all_blogs.html', title="My Blog!", blogs=blogs, endpoint='/myblog')
+
+
+@app.route("/users", methods=['GET'])
+def users():
+    users = User.query.all()
+    return render_template(
+        'all_users.html', title="All Users", users=users, endpoint='/users')
+
+
+@app.route("/userblog", methods=['GET'])
+def userblogs():
+    id = request.args.get('id')
+    owner = User.query.filter_by(id=id).first()
+
+    blogs = Blog.query.filter_by(owner=owner).all()
+    blogs = list(reversed(blogs))
+
+    return render_template(
+        'all_blogs.html',
+        title="{0}'s' Blog!".format(owner.username),
+        blogs=blogs,
+        endpoint='/userblog',
+        owner=owner)
+
 
 @app.route("/post", methods=['GET'])
 def single_post():
@@ -153,6 +223,7 @@ def single_post():
     blog = Blog.query.filter_by(id=id).first()
 
     return render_template('single_post.html', blog=blog, title=blog.name)
+
 
 if __name__ == '__main__':
     app.run()
